0755a03f1d57fc66cd419fe40c8dff0f7fe645f2
KahWee Teng <t@kw.sg>
2015-05-30 14:42:29 +0800

    Code

---
+++ README.md
@@ -0,0 +1,27 @@
+# viewability
+
+browser package to check if the element is on screen.
+
+```js
+var vertical = require('viewability/vertical');
+vertical(document.getElementById('blue-box'));
+// return {value: 1, state: "EL_IS_WITHIN_VERTICAL_VIEW"}
+```
+
+Use `'viewability/horizontal'` for corresponding view.
+
+# I want to find out if element is 100% on screen
+
+```js
+var v = require('viewability');
+var el = document.getElementById('blue-box');
+if (v.horizontal(el).value === 0 && v.vertical(el).value === 0) {
+  console.log('100% on screen.')
+} else if (v.horizontal(el).value > 0 || v.value(el).value > 0) {
+  console.log('Part of element in on screen.')
+}
+```
+
+# License
+
+ISC.
---
+++ vertical.js
@@ -0,0 +1,48 @@
+module.exports = function(el) {
+	var windowHeight = window.innerHeight;
+	var elemTop = el.getBoundingClientRect().top;
+	var elemBottom = el.getBoundingClientRect().bottom;
+	var elemHeight = elemBottom - elemTop;
+	if (elemTop > windowHeight) {
+		// Not viewable, below viewport
+		return {
+			value: 0,
+			state: 'EL_IS_BELOW_VIEW'
+		};
+	} else if (elemBottom <= 0) {
+		// Not viewable, above the viewport
+		return {
+			value: 0,
+			state: 'EL_IS_ABOVE_VIEW'
+		};
+	} else if (elemTop >= 0 && elemBottom <= windowHeight) {
+		// Element is completely visible
+		return {
+			value: 1,
+			state: 'EL_IS_WITHIN_VERTICAL_VIEW'
+		};
+	} else if (elemTop < 0 && elemBottom > windowHeight) {
+		// Top and bottom of element truncated
+		return {
+			value: windowHeight / elemHeight,
+			state: 'EL_BOTTOM_AND_TOP_TRUNCATED'
+		};
+	} else if (elemTop < 0 && elemBottom <= windowHeight) {
+		// Top of element is truncated
+		return {
+			value: elemBottom / elemHeight,
+			state: 'EL_TOP_TRUNCATED'
+		};
+	} else if (elemTop >= 0 && elemBottom > windowHeight) {
+		// Bottom of element is trunctaed
+		return {
+			value: (windowHeight - elemTop) / elemHeight,
+			state: 'EL_BOTTOM_TRUNCATED'
+		};
+	}
+	// Generic error
+	return {
+		value: 0,
+		state: 'EL_IS_NOT_WITHIN_VIEW'
+	};
+};
