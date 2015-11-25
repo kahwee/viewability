4398d298951a12f2426dda138577ef46a7456b0d
KahWee Teng <t@kw.sg>
2015-11-24 22:25:06 -0800

    Eslint fixes for test

--- tests/vertical-spec.js
+++ tests/vertical-spec.js
@@ -1,22 +1,22 @@
 /* jshint ignore:start */
-'use strict';
-$('<div id="vtest" style="background: blue; position: absolute; top: 0; left: 0; height: 40px; width: 40px"></div>').appendTo($(document.body));
+'use strict'
+$('<div id="vtest" style="background: blue; position: absolute; top: 0; left: 0; height: 40px; width: 40px"></div>').appendTo($(document.body))
 
-var vertical = window.vertical = require('../vertical');
-var test = document.getElementById('vtest');
+var vertical = window.vertical = require('../vertical')
+var test = document.getElementById('vtest')
 
-describe('vertical', function() {
-	it('should be start with being visible', function(done) {
-		var result = vertical(test);
-		expect(result.value).to.be.equal(1);
-		done();
-	});
+describe('vertical', function () {
+  it('should be start with being visible', function (done) {
+    var result = vertical(test)
+    expect(result.value).to.be.equal(1)
+    done()
+  })
 
-	it('should be off the screen at the very bottom', function(done) {
-		test.style.top = '5000px';
-		var result = vertical(test);
-		expect(result.value).to.be.equal(0);
-		done();
-	});
-});
+  it('should be off the screen at the very bottom', function (done) {
+    test.style.top = '5000px'
+    var result = vertical(test)
+    expect(result.value).to.be.equal(0)
+    done()
+  })
+})
 /* jshint ignore:end */
