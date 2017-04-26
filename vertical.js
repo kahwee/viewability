07ba507de66113a213c0230f079b81486bdbd2b5
John Mealy <jmealy@nyml-jmealy.chegg.com>
2017-04-26 12:04:20 -0400

    Corrected contents of vertical.js

--- vertical.js
+++ vertical.js
@@ -1,67 +1,67 @@
 /**
-  A method for calculating the percentage of an element inside of a given region of the viewport
+  A method for calculating the percentage of an element inside of a given vertical region of the viewport
   @param {element} el Element from the DOM
   @param {object} config Object containing specifications for the viewability appraisal
-  @param {number} config.offsetRight Number of pixels to offset on the righthand side of the viewport
-  @param {number} config.offsetLeft Number of pixels to offset on the lefthand side of the viewport
+  @param {number} config.offsetTop Number of pixels to offset on the top side of the viewport
+  @param {number} config.offsetBottom Number of pixels to offset on the bottom of the viewport
   @return {object} Object containing a `value` property, denoting the percent of the element inside of the active viewport, and a `state` property consisting of string, denoting the element's in-view status
  */
 
 module.exports = function (el, config = {}) {
-  
-    // Get the cropped righthand boundary
-  const windowWidth = window.innerWidth;
-  const offsetRight = config.offsetRight || 0;
-  const rightBound = windowWidth - offsetRight;
 
-  // Get the cropped lefthand boundary:
-  const leftBound = config.offsetLeft || 0;
+    // Get the cropped bottom boundary
+  const windowHeight = window.innerHeight;
+  const offsetBottom = config.offsetBottom || 0;
+  const bottomBound = windowHeight - offsetBottom;
+
+  // Get the cropped top boundary:
+  const topBound = config.offsetTop || 0;
 
   // Get the cropped viewport size, to later evaluate percent of element in view
-  const activeViewportWidth = rightBound - leftBound;
+  const activeViewportHeight = bottomBound - topBound;
 
   // Get element specs:
   const elemRect = el.getBoundingClientRect();
-  const elemLeft = elemRect.left;
-  const elemRight = elemRect.right;
-  const elemWidth = elemRight - elemLeft;
-  
+  const elemTop = elemRect.top;
+  const elemBottom = elemRect.bottom;
+  const elemHeight = elemBottom - elemTop;
+
   // Evaluate element's position in the cropped viewport:
-  if (elemLeft > rightBound) {
-    // Not viewable, to right of viewport
+  if (elemTop > bottomBound) {
+    // Not viewable, below viewport
     return {
       value: 0,
-      state: 'EL_IS_TOO_RIGHT'
+      state: 'EL_IS_BELOW_VIEW'
     }
-  } else if (elemRight <= leftBound) {
-    // Not viewable, to left of viewport
+  } else if (elemBottom <= topBound) {
+    // Not viewable, above the viewport
     return {
       value: 0,
-      state: 'EL_IS_TOO_LEFT'
+      state: 'EL_IS_ABOVE_VIEW'
     }
-  } else if (elemLeft >= leftBound && elemRight <= rightBound) {
+  } else if (elemTop >= topBound && elemBottom <= bottomBound) {
     // Element is completely visible
     return {
       value: 1,
-      state: 'EL_IS_WITHIN_HORIZONTAL_VIEW'
+      state: 'EL_IS_WITHIN_VERTICAL_VIEW'
     }
-  } else if (elemLeft < leftBound && elemRight > rightBound) {
-    // Left and right of element truncated
+  } else if (elemTop < topBound && elemBottom > bottomBound) {
+    // Top and bottom of element truncated
     return {
-      value: activeViewportWidth / elemWidth,
-      state: 'EL_LEFT_AND_RIGHT_TRUNCATED'
+      value: activeViewportHeight / elemHeight,
+      state: 'EL_BOTTOM_AND_TOP_TRUNCATED'
     }
-  } else if (elemLeft < leftBound && elemRight <= rightBound) {
-    // Left of element is truncated
+  } else if (elemTop < topBound && elemBottom <= bottomBound) {
+    // Top of element is truncated
     return {
-      value: (elemRight - leftBound) / elemWidth,
-      state: 'EL_LEFT_TRUNCATED'
+      value: (elemBottom - topBound) / elemHeight,
+      state: 'EL_TOP_TRUNCATED'
     }
-  } else if (elemLeft >= leftBound && elemRight > rightBound) {
-    // Right of element is trunctaed
+  } else if (elemTop >= topBound && elemBottom > bottomBound) {
+    // Bottom of element is trunctaed
     return {
-      value: (activeViewportWidth - elemLeft) / elemWidth,
-      state: 'EL_RIGHT_TRUNCATED'
+      value: (activeViewportHeight - elemTop) / elemHeight,
+      state: 'EL_BOTTOM_TRUNCATED'
     }
   }
   // Generic error
